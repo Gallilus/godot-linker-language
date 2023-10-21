5fe0bb8818b4f700c4f69e31a7947d062c122489
David Cambré <david.cambre@gmail.com>
2023-10-21 16:19:19 +0200

    Register Script and Language

--- modules/linker/register_types.cpp
+++ modules/linker/register_types.cpp
@@ -1,19 +1,61 @@
 #include "register_types.h"
 
+#include "core/config/engine.h"
 #include "core/object/class_db.h"
-#include "LinkerScript.h"
-#include "summator.h"
+
+#include "language/linker_language.h"
+#include "language/linker_script.h"
+
+using namespace godot;
+
+LinkerLanguage *linker_language = nullptr;
+//LinkerLoader *linker_loader = nullptr;
+//LinkerSaver *linker_saver = nullptr;
 
 void initialize_linker_module(ModuleInitializationLevel p_level) {
-	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-			return;
+	if (p_level == GDEXTENSION_INITIALIZATION_CORE) {
+		// not accesable by extensions
 	}
-	ClassDB::register_class<LinkerScript>();
+	if (p_level == GDEXTENSION_INITIALIZATION_SERVERS) {
+		GDREGISTER_CLASS(LinkerScript);
+
+		linker_language = memnew(LinkerLanguage);
+		ScriptServer::register_language(linker_language);
+
+		//		ClassDB::register_class<LinkerLoader>();
+		//		ClassDB::register_class<LinkerSaver>();
+	}
+	if (p_level == GDEXTENSION_INITIALIZATION_SCENE) {
+		//		linker_saver = memnew(LinkerSaver);
+		//		ResourceSaver::get_singleton()->add_resource_format_saver(linker_saver);
+		//		linker_loader = memnew(LinkerLoader);
+		//		ResourceLoader::get_singleton()->add_resource_format_loader(linker_loader);
+	}
+	if (p_level == GDEXTENSION_INITIALIZATION_EDITOR) {
+	}
+	if (p_level == GDEXTENSION_MAX_INITIALIZATION_LEVEL) {
+	}
 }
 
 void uninitialize_linker_module(ModuleInitializationLevel p_level) {
-	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-			return;
+	if (p_level == GDEXTENSION_INITIALIZATION_CORE) {
 	}
-   // Nothing to do here in this example.
+	if (p_level == GDEXTENSION_INITIALIZATION_SERVERS) {
+	}
+	if (p_level == GDEXTENSION_INITIALIZATION_SCENE) {
+		if (linker_language) {
+			memdelete(linker_language);
+		}
+		/*if (linker_saver) {
+			memdelete(linker_saver);
+		}
+		if (linker_loader) {
+			memdelete(linker_loader);
+		}*/
+	}
+	if (p_level == GDEXTENSION_INITIALIZATION_EDITOR) {
+	}
+	if (p_level == GDEXTENSION_MAX_INITIALIZATION_LEVEL) {
+		//ERR_PRINT("this never prints");
+	}
 }
